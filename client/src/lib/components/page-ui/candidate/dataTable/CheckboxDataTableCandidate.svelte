00dd4b6781e8c433720edf5e64d30161b62332a2
aradhya-tiwari <aradhya.prad@gmail.com>
2025-07-11 09:54:00 +0530

    candidates page

---
+++ client/src/lib/components/page-ui/candidate/dataTable/CheckboxDataTableCandidate.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import type { ComponentProps } from "svelte";
+    import { Checkbox } from "$lib/components/ui/checkbox/index.js";
+
+    let {
+        checked = false,
+        onCheckedChange = (v) => (checked = v),
+        ...restProps
+    }: ComponentProps<typeof Checkbox> = $props();
+</script>
+
+<Checkbox
+    bind:checked={() => checked, onCheckedChange}
+    {...restProps}
+    class="z-20 shadow border border-"
+/>
---
+++ client/src/routes/(app)/app/candidates/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Candidate from "$lib/components/page-ui/candidate/+Candidate.svelte";
+</script>
+
+<h1 class="text-3xl my-5">Candidates</h1>
+
+<Candidate />
